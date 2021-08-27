676af7babb6c21c6bbdb2597bd8f8d1006854db6
shreyas-student-python <shreyasvispute14@gmail.com>
2021-08-27 18:43:58 +0530

    Completed the first chapter

---
+++ 01Basics/01chapter/input_method.py
@@ -0,0 +1,5 @@
+first_name = input("Insert your first name : ")
+last_name = input("Insert your last name : ")
+age = input("Insert your age : ")
+
+print(f"My name is {first_name} {last_name}. I am {age} years old.")
---
+++ 01Basics/01chapter/string_methods_functions.py
@@ -0,0 +1,20 @@
+name = "Kaustubh Wankhede"
+
+# .lower() method
+print(name.lower())
+
+# .upper() method
+print(name.upper())
+
+# .title() method
+print(name.title())
+
+# .count() method
+print(name.count("a"))
+
+# Find and Replace
+
+print(name.replace("K","S"))
+print(name)
+
+print(name.find("W"))
