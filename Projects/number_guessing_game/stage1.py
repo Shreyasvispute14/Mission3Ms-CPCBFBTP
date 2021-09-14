c2b9486f71bf7428bb9bb03491d564760280d6f0
shreyas-student-python <shreyasvispute14@gmail.com>
2021-09-14 18:23:31 +0530

    number guessing game stage1 completed

---
+++ Projects/number_guessing_game/stage1.py
@@ -0,0 +1,16 @@
+print("Welcome to the Number Guessing Game!")
+print("I am Kaustubh. Can you help me to guess a number between 1 to 10 :")
+Number=int(input("Guess a number:"))
+
+if Number == 7:
+    print("You win! Congratulations.")
+  
+if Number < 7 and Number > 0:
+    print("Its low.Try again.") 
+
+if Number > 7 and Number < 10: 
+    print("Its high. Try again.")
+
+else:
+    print("Not Applicable")
+
