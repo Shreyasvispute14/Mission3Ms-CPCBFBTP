8286f80b9a119c72cf32507e2ef4aeeb33cdb312
shreyas-student-python <shreyasvispute14@gmail.com>
2021-09-19 19:29:13 +0530

    List Methods and Functions Completed

---
+++ 01Basics/lists.py
@@ -0,0 +1,99 @@
+# Introduction to Lists
+
+# Lists are ordered collection of elements.
+
+list1 = [
+    "Kaustubh Wankhede",
+    "Shreyas Vispute",
+    "Sankalp Joshi",
+    "Akshay Kumar"
+]
+# print(list1)
+# print(type(list1))
+
+
+
+
+# String Indexing
+name = "Jad Abboud"
+# print(name[4])
+
+
+
+# List Indexing
+fruits = ["Apple", "Apple", "Banana", "Mango", "Orange", "Green Apple", "Kiwi"]
+# print(fruits[3])
+
+
+# Methods : 
+
+# count
+# print(fruits.count("Apple"))
+
+
+# sort - permanent
+# print(fruits)
+# fruits.sort()
+# print(fruits)
+
+
+# clear
+# fruits.clear()
+# print(fruits)
+
+# copy
+# fruits_copy = fruits.copy()
+# print(fruits_copy)
+
+# append
+# fruits.append("Papaya")
+# print(fruits)
+
+
+# insert
+# fruits.insert(2, "Watermelon")
+# print(fruits)
+
+
+# extend
+# fruits2 = ["dragon fruit", "star fruit", "mango the fruit"] 
+# fruits.extend(fruits2)
+# print(fruits)
+
+
+# pop
+# fruits.pop()
+# print(fruits)
+
+
+# delete
+# del fruits[2]
+# print(fruits)
+
+
+# remove
+# fruits.remove("Kiwi")
+# print(fruits)
+
+
+# split
+# names = "alan;joe;john;horrible;terrible;mad;sad"
+# names_list = names.split(";")
+# print(names_list)
+# print(len(names_list))
+
+
+# max
+# numbers = [1,2,3,4,5,6,7,8,9]
+# print(max(numbers))
+
+
+# # min
+# print(min(numbers))
+
+
+# functions :
+# sorted -> temporary
+# print(sorted(fruits))
+# print(fruits)
+
